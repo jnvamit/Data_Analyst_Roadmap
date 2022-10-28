62c1a09375cd0f2b038e2997c35087c19cfd8eb1
jnvamit <jnvamit1666@gmail.com>
2022-10-28 23:02:07 +0530

    completed first two lesson of streamlit

---
+++ Streamlit/streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def streamlit_intro():
+    """function is basic intro to streamlit"""
+    
+    st.title('Hello Streamlit')
+
+def streamlit_text():
+    """function represents texual data usage in streamlit"""
+    st.title('Hello Streamlit')
+    st.header('Header')
+    st.subheader('Sub Header')
+    st.markdown("""
+                    # h1
+                    ## h2
+                    ### h3
+                    :moon:<br>
+                    :sunglasses:
+                    **bold**
+                    _italic_
+                """,True)
+    st.write('# Writing ')
+    
+
+if __name__ == '__main__':
+    streamlit_text()
